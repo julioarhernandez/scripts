7e4a9f97e7dc3ccd471c26867e58771f3699f861
Julio <Julioarhernandez@gmail.com>
2016-11-12 12:49:17 -0500

    Jquery plugin - handling scroll with delays

---
+++ scroll-delay.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+ 
+	$.fn.scrolled = function(callback, delay){
+		var timer = null;
+        var delay = delay || 1000;
+		this.scroll(function(){
+			if (timer){ clearTimeout(timer);}
+			timer = setTimeout(function(){
+      callback.call()}, delay);
+		});
+	} 
+  
+  function doSomething(){
+  	alert('scrolled');
+  }
+  
+  $(window).scrolled(doSomething);
+ 
+}); // ready() END
