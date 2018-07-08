7bf7150b0401ce63e677ca7a40192fcc7afcc0d2
julioarhernandez <julioarhernandez@gmail.com>
2018-07-08 15:04:44 -0400

    Test promise function

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+const axios = require('axios');
+
 module.exports = {
     sayHello: function() {
         return 'hello';
     },
     addNumbers: function(value1, value2) {
         return value1 + value2;
+    },
+    checkAnswers: function(){
+        return axios.get('http://yesno.wtf/api/')
+                    .then(function(resp){
+                        return resp.data.answer;
+                    }).catch(function(error){
+                        return error;
+                    });
     }
+
 };
--- test/appTest.js
+++ test/appTest.js
@@ -1,11 +1,11 @@
 const assert = require('chai').assert;
-// const sayHello = require('../app').sayHello;
-// const addNumbers = require('../app').addNumbers;
+const expect = require('chai').expect;
 const app = require('../app');
 
 // Results
 sayHelloResults = app.sayHello();
 addNumbersResults = app.addNumbers(5, 5);
+checkAnswers = app.checkAnswers();
 
 describe('App', function(){
     describe('Say Hello', function(){
@@ -28,6 +28,20 @@ describe('App', function(){
         it('addNumber shoud return type number', function(){
             // let result = app.addNumbers(1, 1);
             assert.typeOf(addNumbersResults, 'number');
+        });
+    });
+    describe('CheckAnswers objects', function() {
+        it('Should return a promise', function(){
+            expect(typeof checkAnswers).to.equal('object');
+        });
+    });
+    describe('CheckAnswers function as a promise', function () {
+        it('Should return yes or no', function(){
+            return checkAnswers.then( function( answer ){
+                assert.equal((answer == 'yes' ||  answer == 'no'), true);
+            }).catch(function( error ){
+                expect(error).to.be.undefined;
+            }); 
         });
     });
 });
